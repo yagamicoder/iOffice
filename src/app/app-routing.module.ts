df757ba5d2bf4293f7c89ee91993034a19cf9268
spartan082189 <michaelgillam89@gmail.com>
2019-07-12 09:28:20 -0500

    Fixed child routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from '../app/components/profile/profile.component'
+import { DirectoryComponent } from '../app/components/directory/directory.component'
+
 
 const routes: Routes = [
+  { path : '' , component: DirectoryComponent },
   { path: 'user/:id', component: ProfileComponent }
 ];
 
