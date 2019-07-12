6256de0b92daf72b251d11c94772b3ef249051e1
spartan082189 <michaelgillam89@gmail.com>
2019-07-12 09:32:37 -0500

    Update profile.component.ts

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const { id } = params
+      if(id) {
+        //Fetch user profile
+      }
+    })
   }
 
 }
